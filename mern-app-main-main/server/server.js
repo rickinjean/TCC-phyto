9935f855304c4a1e73230feafc93edfc41e08cba
Jean Lucas <Jbettifernandes@gmail.com>
2026-06-08 09:25:33 -0300

    novo5

--- mern-app-main-main/server/server.js
+++ mern-app-main-main/server/server.js
@@ -1,7 +1,6 @@
 const express = require("express")
 const app = express()
 const cors = require("cors")
-const path = require('path');
 
 const port = 5050
 
@@ -12,17 +11,8 @@ app.use(require("./routes/plant")) // cria as rotas para manipulação de planta
 
 const dbo = require("./db/conn")
 
-// 1. Libera o acesso aos seus arquivos .js e .css (da pasta client)
-app.use(express.static(path.join(__dirname, 'client')));
-
-// 2. Se acessar a raiz "/", redireciona direto no navegador para "/plantlist"
 app.get('/', (req, res) => {
-    res.redirect('/Plantlist');
-});
-
-// 3. Quando o navegador for para "/plantlist", o servidor entrega o seu index.html
-app.get('/Plantlist', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'index.html'));
+    res.redirect('/plant'); 
 });
 
 dbo.connectToMongoDB(function (error) {
