117f7ff19283ec42efffa2361779407bc4091864
Jean Lucas <Jbettifernandes@gmail.com>
2026-06-08 09:13:06 -0300

    novo2

--- mern-app-main-main/server/server.js
+++ mern-app-main-main/server/server.js
@@ -12,7 +12,7 @@ app.use(require("./routes/plant")) // cria as rotas para manipulação de planta
 const dbo = require("./db/conn")
 
 app.get("/", function(req, res) {
-    res.send("App is running")
+    res.redirect("/Plantlist")
 })
 
 dbo.connectToMongoDB(function (error) {
